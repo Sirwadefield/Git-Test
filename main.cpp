5fc32e53c63e75b293ff7ca1e6bb564c38f1c497
SirWadefield <wadhwake@ut.utm.edu>
2017-11-30 20:15:45 -0600

    updated main

--- main.cpp
+++ main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-
 using namespace std;
 
-int main()
-{
-    cout << "Hello world!" << endl;
-    return 0;
+
+int sum(int parameter);
+
+int main(){
+  int num;
+  cout << "Enter a number(Q to quit) : ";
+  while(cin >> num)
+    {
+        cout << "The summation of "<< num << " is " << sum(num) << endl;
+        cout << "Enter a number(Q to quit): ";
+    }
+  return 0;
 }
+
+int sum(int parameter)
+{
+  int sum = 0;
+  for(int i = 1; i <= parameter; i++)
+    sum += i;
+  return sum;
+}
+
+
